d2ff827d2b436fd6a45c9761629298df9a64bcd9
sanasdh <sanasdh@gmail.com>
2021-01-15 19:04:42 -0500

    commit

--- src/Api/omdbapi.js
+++ src/Api/omdbapi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // const KEY = process.env.REACT_APP_KEY;
 
 export default axios.create({
-  baseURL: 'http://omdbapi.com/',
+  baseURL: 'https://omdbapi.com/',
   params: {
     apikey: 'd8c6bd26',
   }
